4f00ada525e53fbe7eafb05af4ea9a1d1d752260
Yukio Ohata(Alias) <77034428+asaioe99@users.noreply.github.com>
2022-09-30 08:55:38 +0900

    Update 64180.ino

--- 64180.ino
+++ 64180.ino
@@ -3,6 +3,8 @@
 // ROM/RAM等の割り当て
 // common area 1 はワークエリアとしてスタック等に利用
 
+
+
 // 専用レジスタ定義
 uint16_t reg_pc;
 uint16_t reg_sp;
@@ -16,13 +18,35 @@ uint8_t reg_mmu_cb;  //common base register
 
 /*
 物理アドレスの計算法は、p53を参照せよ
-特に、1MByteを超えるあ癖薄について、桁あふれは無視さえれることに注意
+特に、1MByteを超えるアクセスについて、桁あふれは無視さえれることに注意
 */
 
+// リセット時の動作はこれと異なる
 void init_reg() {
+  // MMU
   reg_mmu_cba = 0xf0; // p51 fig4-3
   reg_mmu_bb = 0x00;  // p54
   reg_mmu_cb = 0x00;  // p54
+
+  // general
+
+  // specific
+  reg_pc = 0x00; //p120
+
+}
+
+// リセット時の動作 BCDEHLIXIYは初期化されない　SPIRは0リセット
+void reset() {
+  // MMU
+  reg_mmu_cba = 0xf0; // p51 fig4-3
+  reg_mmu_bb = 0x00;  // p54
+  reg_mmu_cb = 0x00;  // p54
+
+  // general
+
+  // specific
+  reg_pc = 0x00; //p120
+
 }
 
 void setup() {
